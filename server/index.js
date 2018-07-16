4d34aaca8ee53afc5ca2c586be84bd92dcc2328a
lili ye <lili.ye@qochealth.com>
2018-07-16 10:00:03 -0400

    Added models, routes and services

--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken', accessToken);
-            console.log('refreshToken', refreshToken);
-            console.log('profile', profile)
-        }
-    )
-);
-
-app.get('/auth/google', 
-    passport.authenticate('google', 
-    {
-        scope:['profile', 'email']
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
-);
+)
 
-app.get('/auth/google/callback', passport.authenticate('google'))
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+// process.env.PORT (environment configuration will be passed by heroku at run time, process.env means to get underline environment and tell us which port to use)
+// console.log(process.env)
 
 const PORT = process.env.PORT || 5000;
 
---
+++ server/services/passport.js
@@ -0,0 +1,52 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
+const keys = require('../config/keys');
+
+const User = mongoose.model('user');
+
+// ==> user click google login, 
+// ==> it will go check if it is an existingUser in passport.use(). Then it will need to give user123 agent a token that says the user is user123 
+// ==> Call serializeUser with the user to generate the identifying piece of info. 
+// ==> set cookie with our cookie session key ===> request db data with cookie // ==> take cookie session key and pass it into "deserializeUser" to turn it into a user 
+// ==> then db recognize cookie then return requested data of the user
+
+// serializeUser is to take our user model and put indentifying information into a session
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+// then pull it out in the future to recognise the returning user
+
+// deserializeUser is to take the previouse store session info and mongoose find it in the data base then turn it in to a user object 
+
+
+passport.deserializeUser((id,done)=> {
+    // the "user" we are passing in is from the user return from GoogleStrategy
+    User.findById(id).then(user=> {
+        done(null, user);
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+    (accessToken, refreshToken, profile, done) => {
+        //findOne returns a promise. Find if the user exist in db or else create a new user. 
+        User.findOne({googleId: profile.id})
+            .then((existingUser)=>{
+                if(existingUser){
+                    done(null, existingUser)
+                } else {
+                    //creates a new model instance
+                    new User({
+                        googleId: profile.id
+                    }).save()
+                        .then(user => done(null, user))
+                }
+            })
+        }
+    )
+);
