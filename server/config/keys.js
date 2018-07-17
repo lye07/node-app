f94e4f31082adf628a3ac4bcc43398e0ed343b5d
lili ye <lili.ye@qochealth.com>
2018-07-17 09:14:50 -0400

    updates

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-    googleClientID: '844502903996-bsie3hgfihs2667j5temiinonf1sqtj7.apps.googleusercontent.com',
-    googleClientSecret:'IbUvAxX5YJ-q3FbULx8gXqrZ',
-    mongoURI: 'mongodb://lye07:Christin37e@ds259079.mlab.com:59079/emaily',
-    cookieKey: 'bzlhfauvhzx768ue4g7rakuury8'
-};
+// Logic for choosing environment
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
