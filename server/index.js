45c013647cb32bee146b94643a0ae5c29f5253cd
197115116 <lili.ye@rbc.com>
2018-11-28 09:59:37 -0500

    test

--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -28,3 +28,5 @@ require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+// testing
